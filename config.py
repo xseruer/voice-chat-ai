581765cc1fa5dd802ac67ed38a0f4fd128d1b7d7
xseruer <seruer@qq.com>
2023-08-28 10:56:16 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# picovoice key
+PICOVOICE_API_KEY = '***********************************'
+# 唤醒词检测离线文件地址
+PICOVOICE_MODEL_PATH = './ppn/Hey-Joey_en_mac_v2_2_0.ppn'
+
+# 阿里云NLS创建Token的AKSK
+ALIYUN_ACCESSKEY_ID = 'LTAI**********'
+ALIYUN_ACCESSKEY_SECRET = '************'
+# 阿里云NLS的AppKey
+ALIYUN_NLS_APPKEY = '***********'
+
+# OpenAI的KEY
+OPENAI_KEY = 'sk-****************'
+OPENAI_MODEL = 'gpt-3.5-turbo'
+OPENAI_SERVER = 'https://api.openai.com'
+# 机器人的名字
+ASSISTANT_NAME = 'Joey'
+
+# 高德APPKEY，用于支持实时天气查询
+GAODE_APIKEY = '*******************'
